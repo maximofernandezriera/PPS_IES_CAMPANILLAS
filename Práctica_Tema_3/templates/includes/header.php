5615ba4257517002607c456caf2b2b357472aaa3
legodark <juan.cerezo@outlook.es>
2021-04-10 16:02:55 +0200

    Vulnerabilidad(bug-resolve): Reparada vulnerabilidad sobre la libreria jQuery, actualizando el CDN de terceros que tenia a la oficial de jQuery y cambiando la versión de 1.11.1 a 3.6.0

--- Práctica_Tema_3/templates/includes/header.php
+++ Práctica_Tema_3/templates/includes/header.php
@@ -12,7 +12,7 @@
     <link href="<?php echo BASE_URI; ?>templates/css/bootstrap.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="<?php echo BASE_URI; ?>templates/css/custom.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="<?php echo BASE_URI; ?>templates/js/bootstrap.js"></script>
     <script src="<?php echo BASE_URI; ?>templates/js/ckeditor/ckeditor.js"></script>
       
